04b810d5e02d9024410297ac87029b176113914d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-19 17:28:55 +0100

    ADDED: boot sclang

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ app.on("ready", () => {
 
   mainWindow.loadURL("file://" + __dirname + "/index.html");
 });
+
+function vendor(filepath) {
+  return path.join(__dirname, "./vendor/mac", filepath);
+}
+
+const sc = require("supercolliderjs");
+
+const options = {
+  debug: true,
+  echo: true,
+  sclang: vendor("SuperCollider/MacOS/sclang")
+};
+
+async function bootSclang() {
+  try {
+    const lang = await sc.lang.boot(options);
+    await lang.interpret('s.boot');
+  } catch(err) {
+    console.error(err)
+  }
+};
+
+bootSclang();
